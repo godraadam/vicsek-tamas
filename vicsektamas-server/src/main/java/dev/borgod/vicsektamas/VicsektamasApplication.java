de239b5e0f0b3784e1e96c83dcd690ebb4b6a30c
Godra Adam <godra.adam@gmail.com>
2021-05-10 11:45:23 +0300

    generated spring boot application

---
+++ vicsektamas-server/src/main/java/dev/borgod/vicsektamas/VicsektamasApplication.java
@@ -0,0 +1,13 @@
+package dev.borgod.vicsektamas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicsektamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VicsektamasApplication.class, args);
+	}
+
+}
