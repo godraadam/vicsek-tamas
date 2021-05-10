ec6e109b26caccb71a457d2e8a3fe6f16cc2732b
Godra Adam <godra.adam@gmail.com>
2021-05-10 12:03:28 +0300

    initial setup

--- vicsektamas-server/src/main/java/dev/borgod/vicsektamas/App.java
+++ vicsektamas-server/src/main/java/dev/borgod/vicsektamas/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VicsektamasApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VicsektamasApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
